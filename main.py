4d4dfa14de78de2da189f90f1b8bf74dacc0b21f
Albert-Huh <heeyonghuh@gmail.com>
2023-11-08 11:18:11 -0600

    read and preprocess data

--- main.py
+++ main.py
@@ -1,7 +1,56 @@
 import os
+from setup import Setup as setup
+import preprocessing
 
 def main(self):
-    pass
+    ############### IMPORT DATA & SIGNAL PROCESSING ###############
+    # list of raw data files in local data folder
+    subject_ind = '1'
+    data_folder_path = os.path.join(os.getcwd(), 'data/UT_Experiment_Data/S'+subject_ind)
+    raw_data_list = os.listdir(data_folder_path)
+
+    # get path of EEG montage
+    montage_path = os.path.join(os.getcwd(), 'data/Workspaces_Montages/passive electrodes/BrainCap','BrainCap 64 Channel','BC-64.bvef')
+    # initilize epoch and event lists
+    bv_epochs_list = []
+    et_epochs_list = []
+    bv_theta_epochs_list = []
+    bv_alpha_epochs_list = []
+    bv_beta_epochs_list = []
+    et_theta_epochs_list = []
+    et_alpha_epochs_list = []
+    et_beta_epochs_list = []
+
+    events_list = []
+
+    for file_name in raw_data_list:
+        if file_name.replace('.xdf','.fif') in raw_data_list:
+            print(file_name, 'already has a preprocessed .fif file.')
+        else:
+            print(file_name, 'is not preprocessed.')
+            # create dict of lsl stream info
+            source = ['Brain_Vision', 'ForheadE-tattoo']
+            n_ch = [18, 6]
+            ch_name = [['Fz', 'F3', 'F7', 'C3', 'T7', 'Pz', 'P3', 'P7', 'O1', 'Oz', 'O2', 'P8', 'P4', 'T8', 'C4', 'Cz', 'F8', 'F4'], ['AF8','Fp2','Fp1','AF7','hEOG','vEOG']]
+            ch_type = [['eeg']*18+['eog']*0, ['eeg']*4+['eog']*2]
+            daq_source = {'source': source,'n_ch': [18, 6],'ch_name': ch_name,'ch_type': ch_type}
+            # read .xdf files
+            raw_path = os.path.join(data_folder_path, file_name)
+            # create setup and raws from different systems
+            raw_setup = setup(data_path=raw_path, data_type='xdf',stream_source=daq_source)
+            raw_dict = raw_setup.raw_dict
+
+            # preprocess raws
+            for key in raw_dict.keyskeys():
+                raw = raw_dict[key]
+                # bandpass filtering
+                filters = preprocessing.Filtering(raw=raw, l_freq=1, h_freq=30)
+                raw = filters.external_artifact_rejection(resample=False, notch=False)
+                # interactively annotate bad signal
+                interactive_annot = raw_setup.annotate_interactively(raw=raw)
+                print(interactive_annot)
+                # save preprocessed raw as .fif
+                raw.save(os.path.join(data_folder_path, file_name.replace('.xdf','.fif')))
 
 if __name__ == '__main__':
     main()
