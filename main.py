d05387504721177d69aa6c45ce30227b51b40e57
Albert-Huh <heeyonghuh@gmail.com>
2024-02-22 02:38:58 -0600

    bug fix

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import mne
 import numpy as np
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from matplotlib.colors import TwoSlopeNorm
 from setup import Setup as setup
 import preprocessing
 
@@ -98,10 +101,11 @@ def n_back_analysis():
             # bandpass filtering
             # filters = preprocessing.Filtering(raw=raw, l_freq=1, h_freq=30)
             # raw = filters.external_artifact_rejection(resample=False, notch=False)
-            ica = preprocessing.Indepndent_Component_Analysis(raw, n_components=raw.info['nchan']-2, seed=92)
+            ica = preprocessing.Indepndent_Component_Analysis(raw, n_components=raw.info['nchan']-2, seed=97)
             reconst_raw = ica.perfrom_ICA()
 
-            epochs = mne.Epochs(raw=reconst_raw, events=events, event_id=event_dict,event_repeated='drop', tmin=-0.2, tmax=1.8, preload=True, picks='eeg')
+            tmin, tmax = -0.2, 1.8
+            epochs = mne.Epochs(raw=reconst_raw, events=events, event_id=event_dict,event_repeated='drop', tmin=tmin-0.5, tmax=tmax+0.5, preload=True, picks='eeg', baseline=None)
             # n0_back = epochs['0']
             # n1_back = epochs['1']
             # n2_back = epochs['2']
@@ -115,6 +119,99 @@ def n_back_analysis():
     # concatenate all epochs from different trials
     all_epochs = mne.concatenate_epochs(epochs_list)
     # epoch analysis
+    freqs = np.arange(2, 36)  # frequencies from 2-35Hz
+    vmin, vmax = -1, 1.5  # set min and max ERDS values in plot
+    baseline = (-0.2, 0)  # baseline interval (in s)
+    cnorm = TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)  # min, center & max ERDS
+
+    kwargs = dict(
+        n_permutations=100, step_down_p=0.05, seed=1, buffer_size=None, out_type="mask"
+    )  # for cluster test
+
+    tfr = mne.time_frequency.tfr_multitaper(
+        epochs,
+        freqs=freqs,
+        n_cycles=freqs,
+        use_fft=True,
+        return_itc=False,
+        average=False,
+        decim=2,
+    )
+    tfr.crop(tmin, tmax).apply_baseline(baseline, mode="percent")
+    event_ids = {'0': 0, '1': 1, '2': 2, '3': 3}
+
+    for event in event_ids:
+        # select desired epochs for visualization
+        tfr_ev = tfr[event]
+        fig, axes = plt.subplots(
+            1, 4, figsize=(12, 4), gridspec_kw={"width_ratios": [10, 10, 10, 1]}
+        )
+        for ch, ax in enumerate(axes[:-1]):  # for each channel
+            # positive clusters
+            _, c1, p1, _ = mne.stats.permutation_cluster_1samp_test(tfr_ev.data[:, ch], tail=1, **kwargs)
+            # negative clusters
+            _, c2, p2, _ = mne.stats.permutation_cluster_1samp_test(tfr_ev.data[:, ch], tail=-1, **kwargs)
+
+            # note that we keep clusters with p <= 0.05 from the combined clusters
+            # of two independent tests; in this example, we do not correct for
+            # these two comparisons
+            c = np.stack(c1 + c2, axis=2)  # combined clusters
+            p = np.concatenate((p1, p2))  # combined p-values
+            mask = c[..., p <= 0.05].any(axis=-1)
+
+            # plot TFR (ERDS map with masking)
+            tfr_ev.average().plot(
+                [ch],
+                cmap="RdBu",
+                cnorm=cnorm,
+                axes=ax,
+                colorbar=False,
+                show=False,
+                mask=mask,
+                mask_style="mask",
+            )
+
+            ax.set_title(epochs.ch_names[ch], fontsize=10)
+            ax.axvline(0, linewidth=1, color="black", linestyle=":")  # event
+            if ch != 0:
+                ax.set_ylabel("")
+                ax.set_yticklabels("")
+        fig.colorbar(axes[0].images[-1], cax=axes[-1]).ax.set_yscale("linear")
+        fig.suptitle(f"ERDS ({event}-Back)")
+        plt.show()
+
+    df = tfr.to_data_frame(time_format=None)
+    df.head()
+
+    df = tfr.to_data_frame(time_format=None, long_format=True)
+
+    # Map to frequency bands:
+    freq_bounds = {"_": 0, "delta": 3, "theta": 7, "alpha": 13, "beta": 35, "gamma": 140}
+    df["band"] = pd.cut(
+        df["freq"], list(freq_bounds.values()), labels=list(freq_bounds)[1:]
+    )
+
+    # Filter to retain only relevant frequency bands:
+    freq_bands_of_interest = ["delta", "theta", "alpha", "beta"]
+    df = df[df.band.isin(freq_bands_of_interest)]
+    df["band"] = df["band"].cat.remove_unused_categories()
+
+    # Order channels for plotting:
+    df["channel"] = df["channel"].cat.reorder_categories(("C3", "Cz", "C4"), ordered=True)
+
+    g = sns.FacetGrid(df, row="band", col="channel", margin_titles=True)
+    g.map(sns.lineplot, "time", "value", "condition", n_boot=10)
+    axline_kw = dict(color="black", linestyle="dashed", linewidth=0.5, alpha=0.5)
+    g.map(plt.axhline, y=0, **axline_kw)
+    g.map(plt.axvline, x=0, **axline_kw)
+    g.set(ylim=(-1.5, 1.5))
+    g.set_axis_labels("Time (s)", "ERDS")
+    g.set_titles(col_template="{col_name}", row_template="{row_name}")
+    g.add_legend(ncol=2, loc="lower center")
+    g.fig.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.08)
+
+    plt.show()
+
     all_n0_back = all_epochs['0'].average()
     all_n1_back = all_epochs['1'].average()
     all_n2_back = all_epochs['2'].average()
